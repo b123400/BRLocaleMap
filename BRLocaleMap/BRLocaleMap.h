8fb84c276bcd88d3db3ab4d5ce18bc863fa265be
b123400 <b123400@gmail.com>
2016-01-11 23:55:38 +0900

    Initial commit

---
+++ BRLocaleMap/BRLocaleMap.h
@@ -0,0 +1,13 @@
+//
+//  BRLocaleMap.h
+//  BRLocaleMap
+//
+//  Created by b123400 on 11/1/2016.
+//  Copyright © 2016 b123400. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BRLocaleMap : NSObject
+
+@end
---
+++ BRLocaleMap/BRLocaleMap.m
@@ -0,0 +1,13 @@
+//
+//  BRLocaleMap.m
+//  BRLocaleMap
+//
+//  Created by b123400 on 11/1/2016.
+//  Copyright © 2016 b123400. All rights reserved.
+//
+
+#import "BRLocaleMap.h"
+
+@implementation BRLocaleMap
+
+@end
